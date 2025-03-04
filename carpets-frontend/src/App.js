08059150c3e52f8fb8cc0e0e3d539c8105e1c8f1
Samm Wiltshire <samm94tw@gmail.com>
2025-03-04 13:34:44 +0000

    importing images

---
+++ carpets-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CarpetImages from './components/CarpetImages';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carpets Gallery</h1>
+      <CarpetImages />
+    </div>
+  );
+}
+
+export default App;
